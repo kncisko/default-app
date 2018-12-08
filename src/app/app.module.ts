dba87ff3d4a40bc9e8f9cac03702b0ff4d7e1c77
kncisko <kresimir.novak@gmail.com>
2018-12-08 16:02:28 +0100

    The component top-menu-bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuBarComponent } from './top-menu-bar/top-menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/top-menu-bar/top-menu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-menu-bar',
+  templateUrl: './top-menu-bar.component.html',
+  styleUrls: ['./top-menu-bar.component.sass']
+})
+export class TopMenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
